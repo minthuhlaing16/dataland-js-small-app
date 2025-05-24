195edcd656b05554363850368aa472105c7e79a0
minthuhlaing16 <minthuhlaing786@gmail.com>
2025-05-24 20:01:00 +0630

    l3 progress bar

---
+++ l3progressbar/app.js
@@ -0,0 +1,28 @@
+const getdownloadbtn = document.querySelector(".download-btn");
+
+const getprogressbar = document.querySelector(".progress-bar");
+
+const seturl = "https://linkedin.com";
+
+getdownloadbtn.addEventListener("click", function () {
+  //   console.log("hay");
+
+  let setwidth = 0;
+
+  let setinv = setInterval(progressinc, 10);
+
+  function progressinc() {
+    if (setwidth >= 100) {
+      clearInterval(setinv);
+
+      // redirect
+      window.location.href = seturl;
+    } else {
+      setwidth++;
+
+      getprogressbar.style.width = `${setwidth}%`;
+      getprogressbar.setAttribute("data-inc", `${setwidth}%`);
+      getdownloadbtn.setAttribute("disabled", true);
+    }
+  }
+});
